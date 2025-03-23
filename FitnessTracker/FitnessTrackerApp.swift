ae588d6c72352f7a4137caff44f95fcf667f8900
Ian Kiprotich <Ian.kiprotich@strathmore.edu>
2025-03-23 10:51:37 +0300

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Ian   on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
